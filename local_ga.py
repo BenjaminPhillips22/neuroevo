0cca087dc85bddcad3b9e9bde608271cf65dc2ba
Sangyeop Lee <leesy714@snu.ac.kr>
2018-04-20 16:45:43 +0900

    multithread evaluation

--- local_ga.py
+++ local_ga.py
@@ -2,28 +2,52 @@ import copy
 from ga_model import *
 from utils import RandomGenerator
 from evaluate import evaluate
+import threading
+import time
+
+class workerThread(threading.Thread):
+    def __init__(self, tid, args):
+        threading.Thread.__init__(self)
+        self.tid = tid
+        self.args = args
+
+    def run(self):
+        start = time.time()
+        evaluate_worker(*self.args)
+
+def evaluate_worker(env, model, max_eval, cuda, env_seed, index, results):
+    for i in index:
+        results[i] = evaluate(env, model[i], max_eval=max_eval, cuda=cuda, env_seed=env_seed)
 
 class GA:
     def __init__(self, population, compressed_models=None, cuda=False, seed=None, env_seed=2018):
+        assert seed is not None
         self.population = population
         self.cuda=cuda
         self.env_seed=env_seed
-        if seed is not None:
-            self.rng = RandomGenerator(rng=seed)
-        if self.rng:
-            self.models = [CompressedModel(start_rng=self.rng.generate()) for _ in range(population)] if compressed_models is None else compressed_models
-        else:
-            self.models = [CompressedModel() for _ in range(population)] if compressed_models is None else compressed_models
+        self.rng = RandomGenerator(rng=seed)
+        self.models = [CompressedModel(start_rng=self.rng.generate()) for _ in range(population)] if compressed_models is None else compressed_models
     # Note: the paper says "20k frames", but there are 4 frames per network
     # evaluation, so we cap at 5k evaluations
-    def get_best_models(self, env, max_eval=5000):
+    def get_best_models(self, env, max_eval=5000, num_worker=16):
+        '''
         results = []
         for m in self.models:
-            if self.rng:
-                results.append(evaluate(env, m, max_eval=max_eval, cuda=self.cuda,
-                    env_seed=self.env_seed))
-            else:
-                results.append(evaluate(env, m, max_eval=max_eval, cuda=self.cuda))
+            results.append(evaluate(env, m, max_eval=max_eval, cuda=self.cuda,
+                env_seed=self.env_seed))
+        '''
+        results = [None] * self.population
+        ins_per_th = self.population // num_worker if self.population % num_worker == 0 else self.population//num_worker+1
+        threads = []
+        for tid in range(num_worker):
+            i_start = tid * ins_per_th
+            i_end = (tid+1) * ins_per_th
+            i_end = i_end if i_end < self.population else self.population
+            threads.append(workerThread(tid, (env, self.models, max_eval, self.cuda, self.env_seed, range(i_start,i_end), results)))
+            threads[tid].start()
+        for th in threads:
+            th.join()
+    
         used_frames = sum([r[1] for r in results])
         scores = [r[0] for r in results]
         scored_models = list(zip(self.models, scores))
@@ -41,12 +65,8 @@ class GA:
         # Elitism
         self.models = [scored_models_tr[0][0]]
         for _ in range(self.population - 1):
-            if self.rng:
-                self.models.append(copy.deepcopy(self.rng.choice(scored_models_tr)[0]))
-                self.models[-1].evolve(sigma, self.rng.generate())
-            else:
-                self.models.append(copy.deepcopy(random.choice(scored_models_tr)[0]))
-                self.models[-1].evolve(sigma)
+            self.models.append(copy.deepcopy(self.rng.choice(scored_models_tr)[0]))
+            self.models[-1].evolve(sigma, self.rng.generate())
             
         return median_score, mean_score, max_score, used_frames, scored_models
 
